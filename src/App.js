7b6da397480f9885e93631a991eae2f592b316f0
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-10-01 13:52:24 -0700

    Adds basic coordinates of a month of libraries

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  ZoomableGroup
+} from 'react-simple-maps';
 import USMap from './assets/us-albers-counties.json';
+import { libraries } from './assets/coordinates.js';
 // import USMap from './assets/5m-US-counties.json';
 // import USMap from './assets/10m.json';
 
 function App() {
   const geoURL = USMap;
-
+  console.log(libraries);
   return (
     <div className="App">
       <ComposableMap
@@ -22,6 +29,14 @@ function App() {
             ))
           }
         </Geographies>
+        {libraries.map((library) => (
+          <Marker
+            key={library.longitude + Math.random()}
+            coordinates={[library.longitude, library.latitude]}
+          >
+            <circle r={2} fill="#F00" stroke="#fff" strokeWidth={1} />
+          </Marker>
+        ))}
       </ComposableMap>
     </div>
   );
