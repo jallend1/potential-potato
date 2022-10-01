e461d772d2e7f6c650d380ec872493bafbb88be5
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-10-01 16:05:04 -0700

    Adds a map of North America so the Alaska and Canada libraries don't look absurd

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import {
   Marker,
   ZoomableGroup
 } from 'react-simple-maps';
-import USMap from './assets/us-albers-counties.json';
+// import USMap from './assets/us-albers-counties.json';
 import { libraries } from './assets/coordinates.js';
 // import USMap from './assets/5m-US-counties.json';
 // import USMap from './assets/10m.json';
+import USMap from './assets/north-america.json';
 
 function App() {
   const geoURL = USMap;
@@ -18,8 +19,9 @@ function App() {
       <ComposableMap
         project="geoAlbers"
         projectionConfig={{
-          scale: 800,
-          center: [-98, 35]
+          scale: 400,
+          center: [-80, 40],
+          rotate: [0, 0, 0]
         }}
       >
         <Geographies geography={geoURL}>
@@ -31,6 +33,7 @@ function App() {
         </Geographies>
         {libraries.map((library) => (
           <Marker
+            onClick={() => console.log(library)}
             key={library.longitude + Math.random()}
             coordinates={[library.longitude, library.latitude]}
           >
