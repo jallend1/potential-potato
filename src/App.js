4657fe47e614bd4679f9a724c524b303ca9cdcda
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-10-01 12:47:13 -0700

    Adds a couple different maps and renders the map in the DOM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import USMap from './assets/us-albers-counties.json';
+// import USMap from './assets/5m-US-counties.json';
+// import USMap from './assets/10m.json';
+
 function App() {
+  const geoURL = USMap;
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <ComposableMap
+        project="geoAlbers"
+        projectionConfig={{
+          scale: 800,
+          center: [-98, 35]
+        }}
+      >
+        <Geographies geography={geoURL}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography key={geo.rsmKey} geography={geo} />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
     </div>
   );
 }
