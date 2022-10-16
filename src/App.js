5daaa386b044d3378aab799c3d8431bedcda8eda
Jason Allen <39307371+jallend1@users.noreply.github.com>
2022-10-16 12:51:33 -0700

    Adds (very ugly) tooltip to display library name on hover

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import Map from './Components/Map';
+import ReactTooltip from 'react-tooltip';
 
 function App() {
+  const [tooltipContent, setTooltipContent] = useState('');
   return (
     <div className="App">
-      <Map />
+      <Map setTooltipContent={setTooltipContent} />
+      <ReactTooltip>{tooltipContent}</ReactTooltip>
     </div>
   );
 }
--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -5,7 +5,7 @@ import {
   Marker,
   ZoomableGroup
 } from 'react-simple-maps';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, memo } from 'react';
 import libraryLocations from '../assets/locations.json';
 import USMap from '../assets/north-america.json';
 // const USMap = 'https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json';
@@ -13,7 +13,7 @@ import USMap from '../assets/north-america.json';
 // import USMap from '../assets/5m-US-counties.json';
 // import USMap from '../assets/10m.json';
 
-const Map = () => {
+const Map = ({ setTooltipContent }) => {
   const populateMapRate = 100;
   const timerRef = useRef(null);
   const [mappedLibraries, setMappedLibraries] = useState([]);
@@ -43,41 +43,41 @@ const Map = () => {
   }, [index]);
 
   return (
-    <ComposableMap
-      projection="geoAlbers"
-      projectionConfig={{ scale: 300, center: [0, 100] }}
-    >
-      <ZoomableGroup
-        zoom={position.zoom}
-        onMoveEnd={handleMoveEnd}
-        center={position.coordinates}
-      >
-        <Geographies geography={USMap}>
-          {({ geographies }) =>
-            geographies.map((geo) => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                fill="#FFF"
-                stroke="#06F"
-                strokeWidth={1}
-              />
-            ))
-          }
-        </Geographies>
-        {mappedLibraries.length > 0 &&
-          mappedLibraries.map((library) => (
-            <Marker
-              onClick={() => console.log(library.library)}
-              key={library.longitude + Math.random()}
-              coordinates={[library.longitude, library.latitude]}
-            >
-              <circle r={0.5} fill="#F10" stroke="#fff" strokeWidth={0} />
-            </Marker>
-          ))}
-      </ZoomableGroup>
-    </ComposableMap>
+    <div data-tip="">
+      <ComposableMap projection="geoAlbers">
+        <ZoomableGroup
+          zoom={position.zoom}
+          onMoveEnd={handleMoveEnd}
+          center={position.coordinates}
+        >
+          <Geographies geography={USMap}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#FFF"
+                  stroke="#06F"
+                  strokeWidth={1}
+                />
+              ))
+            }
+          </Geographies>
+          {mappedLibraries.length > 0 &&
+            mappedLibraries.map((library) => (
+              <Marker
+                key={library.longitude + Math.random()}
+                coordinates={[library.longitude, library.latitude]}
+                onMouseEnter={() => setTooltipContent(`${library.name}`)}
+                onMouseLeave={() => setTooltipContent('')}
+              >
+                <circle r={0.5} fill="#F10" stroke="#fff" strokeWidth={0} />
+              </Marker>
+            ))}
+        </ZoomableGroup>
+      </ComposableMap>
+    </div>
   );
 };
 
-export default Map;
+export default memo(Map);
